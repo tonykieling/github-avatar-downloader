7e23a43832e162b26c105e5f330189e5922af915
Tony Kieling <tony.kieling@gmail.com>
2019-01-15 05:40:32 +0000

    done

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,13 @@
 
 
 var request = require('request');
+var fs = require('fs');
+var token = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-var token = require('./secrets.js');
-
+let repoOwnerF = process.argv[2];
+let repoNameF = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -21,13 +23,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+function downloadImageByURL(url, filePath) {
+  const mkdir = './avatars/';
+  if (!fs.existsSync(mkdir)){
+    fs.mkdirSync(mkdir);
+  }
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Status Message: ', response.statusMessage);
+         console.log('Content Type: ', response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpeg'));
+}
+
+
+getRepoContributors(repoOwnerF, repoNameF, function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
   let parsedContent = JSON.parse(result);
   console.log(parsedContent);
 
-  parsedContent.forEach(element => {
-    console.log(element.avatar_url);
+  parsedContent.forEach(user => {
+    downloadImageByURL(user.avatar_url, user.login);
   });
 });
