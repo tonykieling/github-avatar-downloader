2545f3b9bce5405eefa155895e39fb15bd549912
Tony Kieling <tony.kieling@gmail.com>
2019-01-15 04:17:01 +0000

    done

--- download_avatars.js
+++ download_avatars.js
@@ -4,12 +4,15 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var token = require('./secrets.js');
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization': token.GITHUB_TOKEN
     }
   };
 
@@ -18,11 +21,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-
-
-
-
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  let parsedContent = JSON.parse(result);
+  console.log(parsedContent);
+
+  parsedContent.forEach(element => {
+    console.log(element.avatar_url);
+  });
 });
